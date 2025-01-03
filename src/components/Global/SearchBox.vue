181f7476b72df6fe357a9e8842e576bf4856ae98
prontoTheLegend <anasprog8@gmail.com>
2025-01-03 16:45:31 +0300

    first edition without back end I have to add btn to upload a pictuer to product and make Product create pop up better style

--- src/components/Global/SearchBox.vue
+++ src/components/Global/SearchBox.vue
@@ -1,8 +1,14 @@
-<template>
+    <template>
     <div class="g-search-box ">
-        <input type="text" v-model="searchFor" placeholder="Search for item" class="g-search-box_input">
+        <input @focus="openOldSearch" type="text" v-model="searchFor" placeholder="Search for item" class="g-search-box_input">
         <auth-form-btn :label="'Search'" />
         <i @click="openMobileSearch" class=" g-search-box_icon fa fa-search"></i>
+        <transition name="fade" >
+            <div v-if="oldSearch" class="g-search-box_predictable">
+                <p v-for="i in 100" class="g-search-box-predictable_one" :key="i" >first OLD search</p>
+            </div>
+        </transition>
+        <div v-if="oldSearch" @click='closeOldSearch' class="g-bk" ></div>
     </div>
     <transition>
         <div class="g-search-box mobile" v-if="mobileSearch" >
@@ -21,11 +27,18 @@ export default {
     },
     setup(){
         const mobileSearch = ref(false);
+        const oldSearch = ref(false);
         function openMobileSearch(){
             mobileSearch.value = true;
         }
         function closeMobileSearch(){
             mobileSearch.value = false;
+        }
+        function openOldSearch(){
+            oldSearch.value = true;
+        }
+        function closeOldSearch(){
+            oldSearch.value = false;
         }
         const searchFor = ref('');
         return {
@@ -33,6 +46,9 @@ export default {
             openMobileSearch,
             closeMobileSearch,
             searchFor,
+            oldSearch,
+            openOldSearch,
+            closeOldSearch,
         };
     },
 }
